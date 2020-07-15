a45a8fbe8cfa71095b6df67585f9103fa086472d
Elena Margolin <elenamargolin@Elenas-MBP.attlocal.net>
2020-07-15 09:52:31 -0500

    added AllRestaurants view and container, created restaurant card + css

---
+++ src/components/view/AllRestaurantsView.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../../App.css';
+
+const AllRestaurantsView= () => {
+    const style = {
+        color: 'white',
+        textDecoration: 'none'
+      }
+    return (
+        <div className="container">
+            <div className= "nav-bar">
+            <ul>
+                <li><Link style = {style} to="/">Home</Link></li>
+                <li><Link style = {style} to="/restaurants">Our Mission</Link></li>
+            </ul>
+          </div> 
+
+          <div className="zip-input">
+            <input placeholder='Enter zip code'></input>
+            <button>Search</button><br/>
+          </div>
+
+          <div className="restaurant-card">
+            <div>              
+                <h1>Restaurant Name</h1>
+                <p>Cuisine</p>
+                <p>Address</p>
+            </div>
+            <div>
+                <button>Meal</button>
+            </div>
+              
+          </div>
+        </div>
+    );
+}
+
+
+export default AllRestaurantsView;
