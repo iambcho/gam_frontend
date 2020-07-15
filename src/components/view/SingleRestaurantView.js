66f468c58e3129c87762d2d5c82a265d122c39c4
Brittany Cho <bcho@Brittanys-MacBook-Pro.local>
2020-07-15 16:05:19 -0400

    adding basic code to SingleRestaurantView.js

---
+++ src/components/view/SingleRestaurantView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../../App.css';
+
+const AllRestaurantsView= () => {
+
+    return (
+        <div className="container">
+        </div>
+    );
+}
+
+
+export default AllRestaurantsView;
