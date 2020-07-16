58f0944368e03bd00af7fbae672bdfd171b06ae3
Brittany Cho <bcho@Brittanys-MacBook-Pro.local>
2020-07-16 11:30:49 -0400

    made some changes for zip search feature

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,9 +1,6 @@
-// //AUTH
-// export const GET_USER = "GET_USER";
-// export const REMOVE_USER = "REMOVE_USER";
-
 //RESTAURANTS
 export const FETCH_RESTAURANTS = 'FETCH_RESTAURANTS';
 export const CURRENT_RESTAURANT = 'CURRENT_RESTAURANT';
 
-
+//ZIPCODE
+export const CURRENT_ZIPCODE = 'CURRENT_ZIPCODE';
