e75164507fd69b9db8ba28d378b7017e1c7bc6b3
Elena Margolin <elenamargolin@Elenas-MBP.attlocal.net>
2020-07-16 11:26:06 -0500

    rough chatbot, still errors

--- src/components/view/HomePageView.js
+++ src/components/view/HomePageView.js
@@ -61,6 +61,7 @@ const HomePageView = () => {
           </Link></li>
           <li><Link className="nav-link" style={styles.pageSelected} to="/">HOME</Link></li>
           <li><Link className="nav-link" to="/mission">OUR MISSION</Link></li>
+          <li><a className="nav-link" href="http://gam-covid-assistant.mybluemix.net/">HELP</a></li>
         </ul>
       </div>
       <br />
